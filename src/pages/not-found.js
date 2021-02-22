86d684b84d9f811813bda72701c0cd4e044af6df
Piyush Mehta <me@piyushmehta.com>
2021-02-22 22:46:42 +0530

    404 page made

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  useEffect(() => {
+    document.title = "404 - Not Found";
+  }, []);
+  return (
+    <div className="bg-gray-200">
+      <div className="mx-auto max-w-screen-lg">
+        <p className="text-center text-2xl">Not Found</p>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
