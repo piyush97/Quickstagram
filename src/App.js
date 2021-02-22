149ba160a4183865afa570fac5672121844fad8c
Piyush Mehta <me@piyushmehta.com>
2021-02-22 15:27:23 +0530

    routes setup done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+
+const Dashboard = lazy(() => import("./pages/dashboard"));
+const Login = lazy(() => import("./pages/login"));
+const SignUp = lazy(() => import("./pages/signup"));
+const Profile = lazy(() => import("./pages/profile"));
+const NotFound = lazy(() => import("./pages/not-found"));
+
 function App() {
-  return <h1 className="text-red-500">Hello</h1>;
+  return (
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={SignUp} />
+          <Route path={ROUTES.PROFILE} component={Profile} />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
+          <Route path={ROUTES.NOT_FOUND} component={NotFound} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App;
