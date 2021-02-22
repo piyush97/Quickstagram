34e156f4a88d3ffbab736998f19757200291fa14
Piyush Mehta <me@piyushmehta.com>
2021-02-22 22:43:08 +0530

    components

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,10 +1,20 @@
-import React from "react";
+import Header from "components/header";
+import Sidebar from "components/sidebar";
+import Timeline from "components/timeline";
+import React, { useEffect } from "react";
 
 const Dashboard = () => {
+  useEffect(() => {
+    document.title = "Quickstagram";
+  }, []);
   return (
-    <>
-      <div className="container mx-auto">a</div>
-    </>
+    <div className="bg-gray-100">
+      <Header />
+      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+        <Timeline />
+        <Sidebar />
+      </div>
+    </div>
   );
 };
 
