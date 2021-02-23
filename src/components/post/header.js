65466f56ba86ff0c8fd2b02a53d054cb05d48681
Piyush Mehta <me@piyushmehta.com>
2021-02-23 15:06:53 +0530

    Comments container

---
+++ src/components/post/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header({ username }) {
+  return (
+    <div className="flex border-b h-4 p-4 py-8">
+      <div className="flex items-center">
+        <Link to={`/p/${username}`} className="flex items-center">
+          <img
+            className="rounded-full h-8 w-8 flex mr-3"
+            src={`images/avatars/${username}.jpg`}
+            alt={`${username} profile pic`}
+          />
+          <p className="font-bold">{username}</p>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,6 +1,8 @@
 import React, { useRef } from "react";
 import Actions from "./actions";
+import Comments from "./comments";
 import Footer from "./footer";
+import Header from "./header";
 import Image from "./image";
 
 export default function Post({ content }) {
@@ -9,6 +11,7 @@ export default function Post({ content }) {
 
   return (
     <div className="rounded col-span-4 border bg-white mb-16">
+      <Header username={content.username} />
       <Image src={content.imageSrc} caption={content.caption} />
       <Actions
         docId={content.docId}
@@ -17,6 +20,12 @@ export default function Post({ content }) {
         handleFocus={handleFocus}
       />
       <Footer username={content.username} caption={content.caption} />
+      <Comments
+        docId={content.docId}
+        comments={content.comments}
+        posted={content.dateCreated}
+        commentInput={commentInput}
+      />
     </div>
   );
 }
