81a151bd3613378fff3d98c9f8010ee5f81a2c98
Piyush Mehta <me@piyushmehta.com>
2021-02-23 14:33:30 +0530

    like button

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -3,21 +3,20 @@ import Actions from "./actions";
 import Footer from "./footer";
 import Image from "./image";
 
-const Post = ({ content }) => {
+export default function Post({ content }) {
   const commentInput = useRef(null);
   const handleFocus = () => commentInput.current.focus();
+
   return (
     <div className="rounded col-span-4 border bg-white mb-16">
       <Image src={content.imageSrc} caption={content.caption} />
-      <Footer username={content.username} caption={content.caption} />
       <Actions
         docId={content.docId}
         totalLikes={content.likes.length}
         likedPhoto={content.userLikedPhoto}
         handleFocus={handleFocus}
       />
+      <Footer username={content.username} caption={content.caption} />
     </div>
   );
-};
-
-export default Post;
+}
